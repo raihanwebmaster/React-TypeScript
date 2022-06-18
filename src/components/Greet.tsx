088f45578034deec0b1f655b4f8fd866a8a83f5e
raihanwebmaster <raihanemon2015@gmail.com>
2022-06-18 16:40:02 +0200

    setup react typescript

---
+++ src/components/Greet.tsx
@@ -0,0 +1,10 @@
+type GreetProps = {
+    name: string,
+}
+export const Greet = (props:GreetProps) => {
+    return (
+        <div>
+            <h2>Welcome {props.name} ! You have 10 unread message</h2>
+        </div>
+    )
+}
