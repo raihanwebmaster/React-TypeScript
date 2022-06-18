d23adc916f452c4d0193f043ddad5a2cac43ad9e
raihanwebmaster <raihanemon2015@gmail.com>
2022-06-18 17:33:10 +0200

    advance props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import Oscar from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
   const personName = {
@@ -24,9 +27,14 @@ function App() {
   ]
   return (
     <div className="App">
-      <Greet name={"raihan"} messageCount={100} isLoggedIn={false} />
+      <Greet name={"raihan"}  isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status="loading" />
+      <Heading>PlaceHolder Text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+      </Oscar>
     </div>
   );
 }
