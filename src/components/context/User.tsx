4aeb0ab1bd7f9cc7eb172a4225f93545017eff40
raihanwebmaster <raihanemon2015@gmail.com>
2022-06-26 13:31:23 +0200

    useContext hook

---
+++ src/components/context/User.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+export const User = () => {
+  const userContext = useContext(UserContext);
+  const handleLogin = () => {
+    userContext.setUser({
+      name: "Raihan",
+      email: "raihanemon2015@gmail.conm",
+    });
+  };
+  const handleLogout = () => {
+    userContext.setUser(null);
+  };
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
+    </div>
+  );
+};
