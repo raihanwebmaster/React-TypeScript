63f56d3ba43f62368da4323c1898d251f11e5aef
raihanwebmaster <raihanemon2015@gmail.com>
2022-08-10 18:50:12 +0200

    restricting_props

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { Counter } from "./components/class/Counter";
 import { Private } from "./components/auth/Private";
 import { Profile } from "./components/auth/Profile";
 import { List } from "./components/generics/List";
+import { RandomNumber } from "./components/restriction/RandomNumber";
 
 function App() {
   const personName = {
@@ -76,6 +77,7 @@ function App() {
         ]}
         onClick={(item) => console.log(item)}
       />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
